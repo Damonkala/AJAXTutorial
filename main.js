7cfe0c42fe6f721f0d0f27b32755046799c2f3e2
Damon Kalaboukis <damonkala@yahoo.com>
2016-02-29 15:33:26 -0800

    .get

--- main.js
+++ main.js
@@ -2,16 +2,14 @@
 
 $(document).ready(init);
 
-
 function init() {
 	$('#send').click(sendRequest);
 }
 
-
 function sendRequest() {
 	$.ajax({
 		method: 'GET',
-		url: "http://swapi.co/api/poople/1",
+		url: "http://swapi.co/api/people/1",
 		success: function(res) {
 			console.log(res);
 		}, error: function(error) {
@@ -19,6 +17,7 @@ function sendRequest() {
 		}
 	})
 
-
-
+	$.get('http://swapi.co/api/people/1', function(res, status){
+		console.log(res, status);
+	})
 }
